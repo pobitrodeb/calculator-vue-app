7b4076e11d33288db985d922ff0bb67e4b818950
Pobitrodeb <pobitrodn@gmail.com>
2023-04-29 21:35:01 +0600

    style added

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,41 +4,46 @@
   </div>
   <div class="col-md-6">
     <table class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">C</th>
-      <th scope="col"><i class="fa-solid fa-plus"></i>/-</th>
-      <th scope="col"><i class="fa-solid fa-percent"></i></th>
-      <th scope="col"><i class="fa-solid fa-divide"></i></th>
+  <thead >
+    <tr class="bg-primary text-white py-3">
+      <th colspan="4">Codition and Result </th>
     </tr>
   </thead>
   <tbody>
 
     <tr>
+      <th scope="col">C</th>
+      <th scope="col"><i class="fa-solid fa-plus"></i>/-</th>
+      <th scope="col"><i class="fa-solid fa-percent"></i></th>
+      <th scope="col"  class="bg-primary text-white"> <div class="btn "><i class="fa-solid fa-divide"></i></div> </th>
+    </tr>
+
+    <tr>
       <td>7</td>
       <td>8</td>
       <td>9</td>
-      <td><i class="fa-solid fa-x"></i></td>
+      <td class="bg-primary text-white"><div class="btn"> <i class="fa-solid fa-x"></i> </div></td>
+      
     </tr>
 
     <tr>
       <td>4</td>
       <td>5</td>
       <td>6</td>
-      <td><i class="fa-solid fa-minus"></i></td>
+      <td class="bg-primary text-white"> <div class="btn"><i class="fa-solid fa-minus"></i></div> </td>
     </tr>
 
     <tr>
       <td>1</td>
       <td>2</td>
       <td>3</td>
-      <td><i class="fa-solid fa-plus"></i></td>
+      <td class="bg-primary text-white" > <div class="btn"><i class="fa-solid fa-plus"></i></div> </td>
     </tr>
 
     <tr>
       <td colspan="2">0</td>
       <td><i class="fa-solid fa-circle-dot"></i></td>
-      <td><i class="fa-solid fa-equals"></i></td>
+      <td class="bg-primary text-white" > <div class="btn">  <i class="fa-solid fa-equals"></i></div> </td>
     </tr>
 
   </tbody>
@@ -71,4 +76,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
